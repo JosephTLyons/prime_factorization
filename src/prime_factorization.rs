90d863b76a91dfb61100e1cc10d70bb7e083a251
Joseph Lyons <josephtlyons@gmail.com>
2018-11-16 23:50:08 -0500

    Since rust doesn't care where you define functions (unlike C++), it makes more sense to put the helper function below the function that uses it.

--- src/prime_factorization.rs
+++ src/prime_factorization.rs
@@ -1,15 +1,3 @@
-fn get_first_factor (input: u64) -> u64 {
-    let upper_limit: u64 = (input as f64).sqrt() as u64;
-
-    for possible_factor in 2..upper_limit + 1 {
-        if input % possible_factor == 0 {
-            return possible_factor;
-        }
-    }
-
-    return 0;
-}
-
 pub fn collect_prime_factors (mut input: u64) -> Vec<u64> {
     let mut prime_vec: Vec<u64> = Vec::new();
     let mut first_factor: u64 = get_first_factor (input);
@@ -25,4 +13,16 @@ pub fn collect_prime_factors (mut input: u64) -> Vec<u64> {
     }
 
     return prime_vec;
+}
+
+fn get_first_factor (input: u64) -> u64 {
+    let upper_limit: u64 = (input as f64).sqrt() as u64;
+
+    for possible_factor in 2..upper_limit + 1 {
+        if input % possible_factor == 0 {
+            return possible_factor;
+        }
+    }
+
+    return 0;
 }
