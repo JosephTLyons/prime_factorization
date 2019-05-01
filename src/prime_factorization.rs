0db3566e500990cb24d092af6a047291a3d02d6e
Joseph Lyons <josephtlyons@gmail.com>
2019-05-01 04:30:02 -0400

    Added some tests

--- src/prime_factorization.rs
+++ src/prime_factorization.rs
@@ -31,11 +31,23 @@ fn get_first_factor (input: u64) -> u64 {
 mod tests {
     use super::collect_prime_factors;
 
+    // Test some small and large prime numbers
     #[test]
     fn test_1() {
         assert_eq!(0, collect_prime_factors(1).len());
     }
 
+    #[test]
+    fn test_2() {
+        assert_eq!(0, collect_prime_factors(2).len());
+    }
+
+    #[test]
+    fn test_large_prime() {
+        assert_eq!(0, collect_prime_factors(982_451_653).len());
+    }
+
+    // Test some typical non-prime numbers
     #[test]
     fn test_4() {
         assert_eq!(vec![2, 2], collect_prime_factors(4));
